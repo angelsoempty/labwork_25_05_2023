912fe7033d22a6a506ba6c3aa5f96d86c0ce05bc
angelsoempty <denatamanov1@gmail.com>
2023-06-01 17:05:05 +0300

    #2

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
-def get_page_title(html):
+def get_page_images(html):
     soup = BeautifulSoup(html, 'html.parser')
-    title_tag = soup.title
-    if title_tag:
-        return title_tag.string.strip()
-    else:
-        return None
+    image_tags = soup.find_all('img')
+    images = []
+    for img in image_tags:
+        src = img.get('src')
+        images.append(src)
+    return images
+
 url = 'https://www.example.com/'
 response = requests.get(url)
 html = response.text
-title = get_page_title(html)
-if title:
-    print("Заголовок сторінки:", title)
+images = get_page_images(html)
+
+if images:
+    print("Зображення на сторінці:")
+    for image in images:
+        print(image)
 else:
-    print("Заголовок сторінки не знайдено")
+    print("Зображення на сторінці не знайдено")
