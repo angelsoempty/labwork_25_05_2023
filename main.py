f9f212bd2dd8d8ec85f74dd14ec1418adcb07c13
angelsoempty <denatamanov1@gmail.com>
2023-06-01 17:11:25 +0300

    #4

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
-def get_external_links(html, base_url):
-    soup = BeautifulSoup(html, 'html.parser')
-    link_tags = soup.find_all('a')
-    links = []
-    for link in link_tags:
-        href = link.get('href')
-        if href and not href.startswith('#') and not href.startswith('mailto:'):
-            absolute_url = urljoin(base_url, href)
-            links.append(absolute_url)
-    return links
+import re
+
+def count_words(text):
+    cleaned_text = re.sub('<.*?>', '', text)
+    words = cleaned_text.split()
+    return len(words)
+
 url = 'https://www.example.com/'
 response = requests.get(url)
 html = response.text
-external_links = get_external_links(html, url)
-
-if external_links:
-    print("Зовнішні посилання на інші сторінки:")
-    for link in external_links:
-        print(link)
-else:
-    print("Зовнішні посилання на інші сторінки не знайдено")
+soup = BeautifulSoup(html, 'html.parser')
+text = soup.get_text()
+word_count = count_words(text)
+print("Кількість слів у тексті сторінки:", word_count)
